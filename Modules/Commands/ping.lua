451ba779ab2fce5d22484b28d267b6df9a4ece88
coastss <55933271+coastss@users.noreply.github.com>
2020-06-17 22:01:51 -0400

    Update ping.lua

--- Modules/Commands/ping.lua
+++ Modules/Commands/ping.lua
@@ -1,4 +1,6 @@
---// Initializze every new command file with these two variables, otherwise the command will not run.
+--// Initialize every new command file with these variables, otherwise the command will not run. The "Discordia" variable is needed depending on what you are doing.
+--// Optional: local Discordia = _G.Discordia
+local Client = _G.Client
 local Message = _G.Message
 local Arguments = _G.Arguments
 
