ac5ede2258b0c730a45b8838eb730083bcaa3d56
coastss <55933271+coastss@users.noreply.github.com>
2020-06-17 21:47:18 -0400

    Update and rename pong.lua to ping.lua

---
+++ Modules/Commands/ping.lua
@@ -0,0 +1,5 @@
+--// Initializze every new command file with these two variables, otherwise the command will not run.
+local Message = _G.Message
+local Arguments = _G.Arguments
+
+return(message:reply("pong!"))
