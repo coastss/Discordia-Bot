78fdecf5e79df10333c7932d30b73009dbbd4649
coastss <55933271+coastss@users.noreply.github.com>
2020-06-17 21:42:17 -0400

    Update Configuration.lua

--- Configuration.lua
+++ Configuration.lua
@@ -1,31 +1,7 @@
 local Token = "Bot eNzIyNTeYzOTg2Mjg1MTMzOTQ1.Xuk6RA.KMwcFj2ZOpdm-1lc0QdIliktV0U"
 local Prefix = "!"
 
-local ServerId = "710567924926251030"
-local StaffRole = "710567925337161822"
-local MutedRole = "710567924926251035"
-local DonatorRole = "710567924926251033"
-local BuyersRole = "710567924926251032"
-local MemberRole = "710567924926251031"
-local ChangeLogChannel = "710567925337161828"
-local WelcomeAndGoodbyeChannel = "710567925337161829"
-local PurchaseRequestsChannel = "710567925869707297"
-local WhitelistLogsChannel = "710968499962642543"
-
-local Colors = {}
-
 return{
     Token = Token,
-    Prefix = Prefix,
-    ServerId = ServerId,
-    StaffRole = StaffRole,
-    MutedRole = MutedRole,
-    DonatorRole = DonatorRole,
-    BuyersRole = BuyersRole,
-    MemberRole = MemberRole,
-    ChangeLogChannel = ChangeLogChannel,
-    WelcomeAndGoodbyeChannel = WelcomeAndGoodbyeChannel,
-    PurchaseRequestsChannel = PurchaseRequestsChannel,
-    WhitelistLogsChannel = WhitelistLogsChannel,
-    Colors = Colors
+    Prefix = Prefix
 }
