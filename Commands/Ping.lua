e0801c4d97a8d013f9c229e799709bef8bb07a39
coastss <55933271+coastss@users.noreply.github.com>
2020-09-03 22:23:48 -0400

    Update Ping.lua

--- Commands/Ping.lua
+++ Commands/Ping.lua
@@ -1,1 +1,14 @@
+local Discordia = _G.Discordia
+local Client = _G.Client
+local Message = _G.Message
+local Arguments = _G.Arguments
 
+local Configuration = _G.Configuration
+local Utilites = _G.Utilites
+local FS = _G.FS
+
+local AuthorId = Message.author.id
+local Guild = Client:getGuild(Configuration.ServerId)
+local IsDirectMessages = (Message.channel.type == 1 and true or false)
+--// Initialize every new command with the stuff above this comment, you can insert more things from the enviroment table in the main file then insert them by doing. local NewVariable = _G.NewVariable.
+return Message:reply("Pong!")
