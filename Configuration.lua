8ad7c447ac29c3b8c586dd972c0017799e3b0f53
coastss <55933271+coastss@users.noreply.github.com>
2020-09-03 22:21:59 -0400

    Update Configuration.lua

--- Configuration.lua
+++ Configuration.lua
@@ -1,7 +1,17 @@
 local Token = "Bot YourToken"
 local Prefix = "!"
 
-return{
+local Colors = {
+    Red = Discordia.Color.fromRGB(255, 75, 75).value,
+    Orange = Discordia.Color.fromRGB(255, 165, 0).value,
+    Yellow = Discordia.Color.fromRGB(255, 255, 0).value,
+    Green = Discordia.Color.fromRGB(90, 255, 60).value,
+    Blue = Discordia.Color.fromRGB(0, 255, 255).value,
+    Purple = Discordia.Color.fromRGB(100, 0, 255).value,
+    Pink = Discordia.Color.fromRGB(255, 0, 220).value
+}
+
+return {
     Token = Token,
     Prefix = Prefix
 }
