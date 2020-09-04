b7fbd5ec75125b9ac53edaa974f0a6f51540fc7d
coastss <55933271+coastss@users.noreply.github.com>
2020-09-03 22:29:25 -0400

    Delete ping.lua

--- Modules/Commands/ping.lua
+++
@@ -1,7 +0,0 @@
---// Initialize every new command file with these variables, otherwise the command will not run. The "Discordia" variable is needed depending on what you are doing.
---// Optional: local Discordia = _G.Discordia
-local Client = _G.Client
-local Message = _G.Message
-local Arguments = _G.Arguments
-
-return(Message:reply("pong!"))
